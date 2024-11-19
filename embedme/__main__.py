74900ab1a85f091649bcea8787db101bf09a41d2
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-11-19 16:33:04 +1100

    Create venv on demand

--- embedme/__main__.py
+++ embedme/__main__.py
@@ -1,13 +1,15 @@
 import importlib
 import logging
 import os
+import re
 import subprocess
 import sys
 import venv
-from const import DEFAULT_EMBEDME_PATH, __version__
-from embedme.const import get_embedme_package
+from .const import DEFAULT_EMBEDME_PATH, get_embedme_package
 
-_LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+_LOGGER = logging.getLogger("embedme")
 
 
 def check_venv():
@@ -16,6 +18,7 @@ def check_venv():
         return True
 
     # Are we running in a venv?
+    _LOGGER.info("Running in %s", sys.prefix)
     if sys.prefix != sys.base_prefix:
         # if esphome is installed, it's not the right one
         if importlib.util.find_spec("esphome"):
@@ -27,9 +30,17 @@ def check_venv():
             return False
 
         return True
-    # TODO create and populate a venv
-    _LOGGER.error("EmbedMe should be run in a venv (virtual environment)")
     return False
+
+
+def ask(prompt):
+    prompt = re.sub(r"\n +", "\n", prompt)
+    response = input(prompt + " (y/n)? ").strip().lower()
+    if response != "y" and response != "yes":
+        return False
+    return True
+
+
 
 
 def activate_venv():
@@ -40,26 +51,39 @@ def activate_venv():
     py_executable = DEFAULT_EMBEDME_PATH / "bin" / "python"
     if not py_executable.exists(follow_symlinks=True) or not os.access(py_executable, os.X_OK):
         return False
+    embedme_main = DEFAULT_EMBEDME_PATH / "bin" / "embedme"
+    if not embedme_main.exists(follow_symlinks=True):
+        return False
+
     _LOGGER.info("Activating EmbedMe venv in %s", DEFAULT_EMBEDME_PATH)
-    os.execv(py_executable, [py_executable, "-m", "embedme", *sys.argv[1:]])
+    os.execv(py_executable, ["embedme", "-m", "embedme", *sys.argv[1:]])
+
 
 def create_venv():
-    _LOGGER.info("EmbedMe requires a venv to run, a custom venv can be created in %s", DEFAULT_EMBEDME_PATH)
-    response = input("Do you want to create a venv(y/n)?")
-    if response.lower() != "y" or response.lower() != "yes":
+    if not ask(f"""
+            EmbedMe requires a venv to run; a custom venv can be created in this folder:
+
+            {DEFAULT_EMBEDME_PATH}.
+
+            Do you want to create a venv in that location for future use"""):
         return False
+
+    if DEFAULT_EMBEDME_PATH.exists():
+        if not ask(
+                f"""
+            Folder {DEFAULT_EMBEDME_PATH} already exists, do you want to overwrite it"""
+        ):
+            return False
     builder = venv.EnvBuilder(
         with_pip=True,
-        clear=False,
+        clear=True,
         symlinks=True,
     )
     builder.create(DEFAULT_EMBEDME_PATH)  # type: ignore
-    _LOGGER.info(
-        "EmbedMe venv created in %s, now installing the EmbedMe version of esphome", DEFAULT_EMBEDME_PATH
-    )
+    _LOGGER.info("EmbedMe venv created in %s", DEFAULT_EMBEDME_PATH)
+    _LOGGER.info("Installing EmbedMe in %s", DEFAULT_EMBEDME_PATH)
+    os.environ["PATH"] = f"{DEFAULT_EMBEDME_PATH}/bin:{os.environ['PATH']}"
     os.environ["VIRTUAL_ENV"] = str(DEFAULT_EMBEDME_PATH)
-    os.environ["PATH"] = str(DEFAULT_EMBEDME_PATH / "bin") + os.pathsep + os.environ["PATH"]
-
     subprocess.run(["python", "-m", "pip", "install", get_embedme_package()])
     return True
 
@@ -78,10 +102,13 @@ def run_embedme():
 
 
 def main():
-    if not check_venv() and not activate_venv():
+    if check_venv():
+        return run_embedme()
+    activate_venv()
+    # if activate_venv returns, it was not activated
+    if not create_venv():
         return 1
-    if not create_venv() or not activate_venv():
-        return 1
+    activate_venv()
     return run_embedme()
 
 
